da682b3713eec8dad67db8f02dad6101ffd33220
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 17:51:56 +0500

    feat: update number-analyzer.py to collect and analyze user input for 5 numbers

--- Challenges by ChatGPT/1. number-analyzer.py
+++ Challenges by ChatGPT/1. number-analyzer.py
@@ -2,6 +2,24 @@
 # Number Analyzer — Ask the user for 10 numbers. At the end, display how many were positive, negative, zero, even, and odd. Also display the largest and smallest number. Do not use max() or min().
 # ⭐⭐
 
-number=[]
-for i in range(11):
+
+numbers=[]
+for i in range(5):
     number=int(input("Enter a number: "))
+    numbers.append(number)
+print(numbers)
+
+posnum=0
+negnum=0
+zernum=0
+
+for i in range(5):
+    if numbers[i] > 0:
+        posnum+=1
+
+    elif numbers[i] < 0:
+        negnum+=1
+    else:
+        zernum+=1
+
+print("There are",posnum,"Positive Numbers,",negnum,"Negative Numbers, and",zernum,"Zero Numbers in the list.")
