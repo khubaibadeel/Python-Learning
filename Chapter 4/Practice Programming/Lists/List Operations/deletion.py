3392ae906a637b63034aed4ae0b523e73f9539b4
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 13:35:02 +0500

    feat: enhance deletion example with index removal and additional comments

--- Chapter 4/Practice Programming/Lists/List Operations/deletion.py
+++ Chapter 4/Practice Programming/Lists/List Operations/deletion.py
@@ -1,5 +1,12 @@
 #    2. Deletion:
 #           i) Removing by Value:
+#                   Use remove() function to remove the occurence of an element.
 students =["Khubaib", "Ahmed", "Ali", "Usman", "Bilal"]
 students.remove("Khubaib")
-print(students)
+print(students)     # ['Ahmed', 'Ali', 'Usman', 'Bilal']
+
+#           ii) Removing by Index:
+#                   Use pop() function along with the specific index of the element to be removed. 
+subjects = ["English", "Urdu", "Math", "CS", "Physics", "Islamiat"] 
+subjects.pop(3)   # Removed CS from the list.
+print(subjects)
