38fcd657ade78498963eee0f7f0fcc89d2cf48f7
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 13:18:48 +0500

    feat: add deletion.py for demonstrating list deletion operations

---
+++ Chapter 4/Practice Programming/Lists/List Operations/deletion.py
@@ -0,0 +1,4 @@
+#    2. Deletion:
+#           i) Removing by Value:
+students.remove("Sample")
+print(students)
--- Chapter 4/Practice Programming/Lists/lists.py
+++ Chapter 4/Practice Programming/Lists/lists.py
@@ -3,11 +3,4 @@
     # "List is a data structure in Python used to store multiple Pieces of Data in a specific Sequence."
     # --> Each piece of Data is known as Item/Element.
 
-
-
-
-
-#    2. Deletion:
-#           i) Removing by Value:
-students.remove("Sample")
-print(students)
+ExampleList = ["Ali", "Ibrahim", 1, "Asif", "Rizwan"]
