e54d24007f70d067b86f062ea4797dd5d8da9b9b
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 13:42:12 +0500

    feat: add initial implementation of searching.py for list presence check

---
+++ Chapter 4/Practice Programming/Lists/List Operations/searching.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "mango", "orange", "grape", "banana"]
+
+
+
+if "Apple" in fruits:
+    print("apple is present in the list")
+else:
+    print("apple is not present in the list")
