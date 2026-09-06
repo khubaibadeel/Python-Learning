e8d40bd801e36d1e6bf458db1d8a1e1d42a136ff
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 15:54:10 +0500

    feat: add functionality to check presence of a user-defined number in a list

--- Chapter 4/Practice Programming/Lists/List Operations/searching.py
+++ Chapter 4/Practice Programming/Lists/List Operations/searching.py
@@ -1,8 +1,16 @@
 fruits = ["apple", "banana", "mango", "orange", "grape", "banana"]
 
-
-
 if "Apple" in fruits:
     print("apple is present in the list")
 else:
     print("apple is not present in the list")
+
+random_numbers = [42, 7, 19, 88, 3, 56, 24, 91, 15, 68, 34, 39, 73, 9, 47, 81, 26, 60, 12, 95]
+n = int(input("Enter a number to find: "))
+
+if n in random_numbers:
+    print(n,"is present in the list")
+else:
+    print(n,"is not present in the list")
+
+m=input("Press any key ...")
