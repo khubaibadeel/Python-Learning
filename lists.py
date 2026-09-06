723bf6d43bd84d783f6bae2ac528630d85d3ad82
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 11:06:38 +0500

    feat: enhance list properties documentation with dynamic size explanation

--- lists.py
+++ lists.py
@@ -7,3 +7,6 @@
 list =["Khubaib", "Ahmed", "Ali", "Usman", "Bilal"] # List created
 print(list)   #  printed the list
 
+# List Properties:-
+#    --> Dynamic Size:
+#               The List in Python can change its size. U add/remove items without any problem. List automatically Adjusts its Size.
