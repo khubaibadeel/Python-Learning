e8e7b228acad66c2a5620d72b46fc7f955c575a7
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 13:35:28 +0500

    fix: correct formatting of print statement in deletion example

--- Chapter 4/Practice Programming/Lists/List Operations/deletion.py
+++ Chapter 4/Practice Programming/Lists/List Operations/deletion.py
@@ -8,5 +8,5 @@ print(students)     # ['Ahmed', 'Ali', 'Usman', 'Bilal']
 #           ii) Removing by Index:
 #                   Use pop() function along with the specific index of the element to be removed. 
 subjects = ["English", "Urdu", "Math", "CS", "Physics", "Islamiat"] 
-subjects.pop(3)   # Removed CS from the list.
-print(subjects)
+subjects.pop(3)    # Removed "CS" from the list.
+print(subjects)    #['English', 'Urdu', 'Math', 'Physics', 'Islamiat']
