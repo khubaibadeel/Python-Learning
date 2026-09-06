1c9ab55c3059373dfe11b43829aaaa4b764fd190
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 17:15:14 +0500

    feat: add initial content to applications-of-lists.py for list applications overview

---
+++ Chapter 4/Practice Programming/Lists/applications-of-lists.py
@@ -0,0 +1,6 @@
+# Applications of Lists:-
+#   1. Data Storage and Manipulation:
+#                       List allow us to easily store, process and delete the collection of data.
+
+#   2. Stack & Queue Implementations:
+#                       Lists can be used to implement stacks(LIFO) and Queue(FIFO) data structures, which are important for Algorithms and tasks in Computing.
