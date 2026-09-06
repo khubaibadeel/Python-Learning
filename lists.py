042c9735a0179fde08dbb2b0ed5fe86c5077d575
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 10:58:10 +0500

    feat: add documentation comments for list data structure and creation

--- lists.py
+++ lists.py
@@ -1,2 +1,8 @@
+
+# List:-
+    # "List is a data structure in Python used to store multiple Pieces of Data in a specific Sequence."
+    # --> Each piece of Data is known as Element.
+
+# List Creation:-
 list =["Khubaib", "Ahmed", "Ali", "Usman", "Bilal"] # List created
 print(list)   # List Printed on the Output Screen
