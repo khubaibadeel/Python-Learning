d7da8c24b30a321c55a21ce02fd7bec562d22930
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 13:11:17 +0500

    fix: move list properties documentation to list-properties.py for better organization

--- Chapter 4/Practice Programming/Lists/lists.py
+++ Chapter 4/Practice Programming/Lists/lists.py
@@ -6,18 +6,6 @@
 
 
 
-# List Properties:-
-#    --> Dynamic Size:
-#               The List in Python can change its size. U add/remove items without any problem. List automatically Adjusts its Size.
-
-#    --> Index-Based Access:
-#               Every item in a list has a position called index. First has 0, second has 1 and so on. These index can be used to get a specific item from the list.
-
-#    --> Ordered Collection:
-#               The order in which you add items to the list is preserved. Each time u append a new item it gets its new order/index automatically.
-
-
-
 # List Operations:-
 #    1. Insertion: U can insert an item at different positions in a list using insert() function.
 
