74240ddcd9b96c0efcc9887298494ed13e81fafa
Khubaib295 <khubaibadeelkamboh@gmail.com>
2026-09-06 13:17:15 +0500

    refactor: move insertion example to insertion.py for better organization

---
+++ Chapter 4/Practice Programming/Lists/List Operations/insertion.py
@@ -0,0 +1,7 @@
+
+#    1. Insertion: U can insert an item at different positions in a list using insert() function.
+
+students =["Khubaib", "Ahmed", "Ali", "Usman", "Bilal"]
+students.insert(0,"Sample")
+print(students) # ['Sample', 'Khubaib', 'Ahmed', 'Ali', 'Usman', 'Bilal']
+
--- Chapter 4/Practice Programming/Lists/lists.py
+++ Chapter 4/Practice Programming/Lists/lists.py
@@ -6,13 +6,6 @@
 
 
 
-# List Operations:-
-#    1. Insertion: U can insert an item at different positions in a list using insert() function.
-
-students =["Khubaib", "Ahmed", "Ali", "Usman", "Bilal"]
-students.insert(0,"Sample")
-print(students) # ['Sample', 'Khubaib', 'Ahmed', 'Ali', 'Usman', 'Bilal']
-
 
 #    2. Deletion:
 #           i) Removing by Value:
